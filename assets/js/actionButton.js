8b7c7b600aaa46def290f75a484714f6accbdb9e
Arthur <153244771+ArthurAGomes@users.noreply.github.com>
2024-11-08 11:56:41 +0000

    links e ajustes imagem

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -1,42 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Ajusta para garantir que o site comece no topo ao recarregar
-    if (window.location.hash !== '#inicio') {
-        window.location.hash = '#inicio';
+document.addEventListener("DOMContentLoaded", function () {
+  // Ajusta para garantir que o site comece no topo ao recarregar
+  if (window.location.hash !== "#inicio") {
+    window.location.hash = "#inicio";
+  }
+
+  const video = document.getElementById("drive");
+  const whatsappButton = document.getElementById("whatsappButton");
+
+  // Função para exibir ou esconder os botões ao rolar a página
+  function toggleButtons() {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 100) {
+      video.style.display = "flex";
+      whatsappButton.style.display = "flex";
+      setTimeout(() => {
+        video.style.opacity = "1";
+        whatsappButton.style.opacity = "1";
+      }, 10);
+    } else {
+      video.style.opacity = "0";
+      whatsappButton.style.opacity = "0";
+      setTimeout(() => {
+        video.style.display = "none";
+        whatsappButton.style.display = "none";
+      }, 500);
     }
+  }
 
-    const backToTopButton = document.getElementById('backToTop');
-    const whatsappButton = document.getElementById('whatsappButton');
+  // Adiciona o evento de scroll para exibir ou esconder os botões
+  document.addEventListener("scroll", toggleButtons);
+  toggleButtons(); // Verificação inicial ao carregar a página
 
-    // Função para exibir ou esconder os botões ao rolar a página
-    function toggleButtons() {
-        const scrollPosition = window.scrollY;
-        if (scrollPosition > 100) {
-            backToTopButton.style.display = 'flex';
-            whatsappButton.style.display = 'flex';
-            setTimeout(() => {
-                backToTopButton.style.opacity = '1';
-                whatsappButton.style.opacity = '1';
-            }, 10);
-        } else {
-            backToTopButton.style.opacity = '0';
-            whatsappButton.style.opacity = '0';
-            setTimeout(() => {
-                backToTopButton.style.display = 'none';
-                whatsappButton.style.display = 'none';
-            }, 500);
-        }
-    }
+  // Ação ao clicar no botão do WhatsApp
+  whatsappButton.addEventListener("click", function () {
+    window.open(
+      "https://www.instagram.com/green_tech.ni?igsh=MTFrMHM2dTN0NzVzeA==",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
 
-    // Adiciona o evento de scroll para exibir ou esconder os botões
-    document.addEventListener('scroll', toggleButtons);
-
-    // Ação ao clicar no botão "Voltar ao Topo"
-    backToTopButton.addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-
-    // Ação ao clicar no botão do WhatsApp
-    whatsappButton.addEventListener('click', function () {
-        window.location.href = 'https://wa.me/5599999999999';
-    });
+  // Ação ao clicar no botão do vídeo
+  video.addEventListener("click", function () {
+    window.open(
+      "https://drive.google.com/drive/folders/1-DdCHTiDm-7I9LZ-bvVV4VhVwqV8bnwW",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
 });
